d3803a3cfc1601e79e00ed7eb329fcbd4b64a254
albertpumarola <albert.pumarola.peris@gmail.com>
2019-06-07 10:52:53 +0200

    up prepare

--- prepare_dataset/select_samples.py
+++ prepare_dataset/select_samples.py
@@ -8,7 +8,7 @@ from shutil import copyfile
 parser = argparse.ArgumentParser()
 parser.add_argument('-ii', '--input_bags_dir', type=str, default='/home/lab/Documents/aeroarms_dataset/pos/color', help='Input images directory')
 parser.add_argument('-oi', '--output_bags_dir', type=str, default='/home/lab/Documents/aeroarms_dataset_processed/pos/color', help='Output images directory')
-parser.add_argument('-n', '--every_n_frames', type=int, default=3, help='Select every n frames')
+parser.add_argument('-n', '--every_n_frames', type=int, default=5, help='Select every n frames')
 args = parser.parse_args()
 
 def main():
@@ -25,7 +25,7 @@ def main():
         for i, image_filename in enumerate(images_filenames):
             if i % args.every_n_frames == 0:
                 src_filename = os.path.join(bag_dir, os.path.basename(image_filename))
-                save_filename = "%s_%s" % (dir_name, os.path.basename(image_filename))
+                save_filename = "%s_close_%s" % (dir_name, os.path.basename(image_filename))
                 save_filename = os.path.join(save_dir, save_filename)
                 copyfile(src_filename, save_filename)
 
